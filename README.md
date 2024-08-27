e6737a282d229a83c36f01e54d0a4c4cd602397f
Sebi5000 <sebi.essling@gmail.com>
2024-08-27 16:41:13 +0200

    Add config.toml file for easier configuration

--- README.md
+++ README.md
@@ -8,22 +8,11 @@ Make sure python is installed on your computer in Version > 3.0, then clone the 
 
 After installing the packages you need to do the following:
 1. Create a Folder named "data" within the main folder (Yoda/data)
-2. Place your custom data into that folder e.g. PDF-Files or other File Data
+2. Place your custom data into that folder e.g. PDF-Files or other File Data (it's mandatory to place at least 1 File)
 3. Create a file .env within the main folder and setup following variables:
 
 OPENAI_API_KEY=[YOUR API KEY]<br>
 ANTHROPIC_API_KEY=[YOUR API KEY]<br>
-ANTHROPIC_MODELL=[YOUR MODELL]<br>
-COMPANY_NAME=[The Company Name is used to become a greet in the initial message]<br>
-
->Notice: You can choose one of the listed Anthropic Modells. If you don't have or want a Anthropic API-Key you can remove the Anthropic Variables from .env file. In that case GPT 3.5 is used as default.
-
-You can choose between the following modells (starts with low-price/fast):
-- gpt-3.5-turbo (used if your .env file doesn't contain Anthropic Values)
-- claude-3-haiku-20240307
-- claude-3-sonnet-20240229
-- claude-3-5-sonnet-20240620
-- claude-3-opus-20240229
 
 ## Start the App
 Make sure you have some file in your data Folder. Then run command:
@@ -31,3 +20,15 @@ Make sure you have some file in your data Folder. Then run command:
 ``streamlit run main.py``
 
 If everything is setup correctly the browser will open a Chat on localhost. Notice that the initial load will need some time, because the index is created. If you change your data over time, you can simply delete the /storage folder which is created by the app. This will force the app to recreate the index.
+
+## Further Configuration
+You will find a config.toml file within the apps main folder. Use this file to configure the app texts and modell in more detail:
+
+>Notice: You can choose one of the listed Anthropic Modells. If you don't have or want a Anthropic API-Key you can remove the Anthropic Key from .env file and set the modell parameter in config.toml = "". In that case GPT 3.5 is used as default.
+
+You can choose between the following modells (starts with low-price/fast):
+- gpt-3.5-turbo (used if your .env file doesn't contain Anthropic Values)
+- claude-3-haiku-20240307
+- claude-3-sonnet-20240229
+- claude-3-5-sonnet-20240620
+- claude-3-opus-20240229
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from llama_index.llms.anthropic import Anthropic
 from llama_index.core import Settings
+from tomli import load
+
 from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
@@ -10,33 +12,41 @@ from llama_index.core import (
     load_index_from_storage,
 )
 
-#Setup API Keys and Config Variables
+#LOAD Secrets & API-Keys
 load_dotenv()
-
 if os.getenv("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 if os.getenv("ANTHROPIC_API_KEY"):
     os.environ["ANTHROPIC_API_KEY"] = os.getenv("ANTHROPIC_API_KEY")
-if os.getenv("ANTHROPIC_MODELL"):
-    anthropic_modell = os.getenv("ANTHROPIC_MODELL") #claude-3-haiku-20240307 = fast/cheap, claude-3-sonnet-20240229, claude-3-5-sonnet-20240620, claude-3-opus-20240229 = slow/expensive
+
+#LOAD Configuration
+with open("config.toml", mode="rb") as handle:
+    config = load(handle)
+
+if config["app"]["model"]:
+    anthropic_modell = config["app"]["model"] #os.getenv("ANTHROPIC_MODELL") #claude-3-haiku-20240307 = fast/cheap, claude-3-sonnet-20240229, claude-3-5-sonnet-20240620, claude-3-opus-20240229 = slow/expensive
 else:
     anthropic_modell = "" #will force app to use OpenAI Modell
-if os.getenv("COMPANY_NAME"):
-    company_name = os.getenv("COMPANY_NAME")
-else:
-    company_name = "mindsquare AG"
+
+#Initialize Texts
+title = config["app"]["title"]
+initial_message = config["app"]["texts"]["initial_message"]
+source = config["app"]["texts"]["source"]
 
 #Setup global Settings for the App
 if anthropic_modell:
     Settings.tokenizer = Anthropic().tokenizer
     Settings.llm = Anthropic(model=anthropic_modell)
 
-#Create and persist index based on data folder
+#Create and persist index based on data folder and websites
 PERSIST_DIR = "./storage"
 if not os.path.exists(PERSIST_DIR):
-    # load the documents and create the index
+
+    # Load DATA Folder
     documents = SimpleDirectoryReader("data").load_data()
+
     index = VectorStoreIndex.from_documents(documents)
+    
     # store it for later
     index.storage_context.persist(persist_dir=PERSIST_DIR)
 else:
@@ -46,18 +56,18 @@ else:
 
 # Define a simple Streamlit app for Frontend
 st.set_page_config(
-    page_title="mindsquare Yoda",
+    page_title=title,
     page_icon=""
 )
 
 st.logo("assets/logo.png")
-st.title('mindsquare Yoda')
+st.title(title)
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": f"Frage mich etwas über {company_name}.",
+            "content": f"{initial_message}",
         }
     ]
 
@@ -71,9 +81,7 @@ if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
         chat_mode="condense_question", verbose=False, streaming=True
     )
 
-if prompt := st.chat_input(
-    "Stelle eine Frage"
-):  # Prompt for user input and save to chat history
+if prompt := st.chat_input():  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
 for message in st.session_state.messages:  # Write message history to UI
@@ -88,7 +96,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
 
         #Add Source Information
         with st.container():
-            st.write("Quellen")
+            st.write(source)
             for source in response_stream.source_nodes:
                 st.markdown(source.metadata["file_name"] + " S." + source.metadata["page_label"])
 
